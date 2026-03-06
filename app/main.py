a3360e62ca0a9739082d803447a43931b1b4d1ac
riddhesh_misal <riddhesh.misal@talentica.com>
2026-03-06 13:41:23 +0530

    added on startup event ensuring the database is connected

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+
 from app.product.api import router as product_router
 from app.category.api import router as category_router
 from app.sku.api import router as sku_router
+
 from app.core.database import Base, engine
 from app.product.exceptions import ProductException
 
+
 def create_app() -> FastAPI:
     app = FastAPI(
-        title = "Inventory Management Service",
-        version = "1.0.0"
+        title="Inventory Management Service",
+        version="1.0.0"
     )
 
     @app.exception_handler(ProductException)
@@ -19,12 +22,18 @@ def create_app() -> FastAPI:
             content={"message": exc.message}
         )
 
-    Base.metadata.create_all(bind=engine)
+    @app.on_event("startup")
+    def startup_event():
+        with engine.connect():
+            pass
+
+        Base.metadata.create_all(bind=engine)
 
     app.include_router(category_router)
     app.include_router(product_router)
     app.include_router(sku_router)
 
     return app
+
 
 app = create_app()
